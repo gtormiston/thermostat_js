3cc599f0f3e344f57b1d16e0a02ee0bde982c4be
Alex-Swann <alex_swann@hotmail.co.uk>
2016-06-22 17:08:17 +0100

    jquery utilised with ajax to access weather api

--- js/interface.js
+++ js/interface.js
@@ -22,6 +22,16 @@ $(document).ready(function(){
   function updateTemp(){
     $('#temperature').text(thermo.temp());
     $('#temperature').attr('class', thermo.energyUsage());
-  };
+  }
+
+  $('#current-city').change(function() {
+    var city = $('#current-city').val();
+    var api = 'http://api.openweathermap.org/data/2.5/weather?q=';
+    var key = '&appid=f5ceec348230deb171e07ab0c5a85862&units=metric';
+    $.get(api + city + key, function(data) {
+    $('#current-temperature').text(data.main.temp);
+    });
+  });
+
 
 });
